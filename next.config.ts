3ee3d3515fc92051cb3d3117d6b918be0422c5a2
arijitb17 <arijitb017@gmail.com>
2025-07-31 15:33:57 +0530

    css

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { resolve } from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -7,14 +8,21 @@ const nextConfig: NextConfig = {
       'ibb.co',
       'i.ibb.co',
       'drive.google.com',
-      'res.cloudinary.com' 
+      'res.cloudinary.com',
     ],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.googleusercontent.com", 
+        hostname: "**.googleusercontent.com", // Gmail avatars
       },
     ],
+  },
+  webpack(config, _options) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@tailwindcss/postcss': resolve(__dirname, 'node_modules', 'tailwindcss'),
+    };
+    return config;
   },
 };
 
